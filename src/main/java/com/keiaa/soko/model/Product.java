70baa529ab24ab5e0778b9d3fcff2ee5a52f19a8
Keiaa <confinedrose@duck.com>
2025-08-04 01:07:09 +0800

    Establish POJO model.

---
+++ src/main/java/com/keiaa/soko/model/Product.java
@@ -0,0 +1,35 @@
+package com.keiaa.soko.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Product() {}
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public int getQuantity() { return quantity; }
+    public double getPrice() { return price; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public void setPrice(double price) { this.price = price; }
+}
