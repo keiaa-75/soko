070c46beb3b70371d2e89be42c111229fc4901e1
Keiaa <confinedrose@duck.com>
2025-08-04 01:06:50 +0800

    Initial commit, start test project.

---
+++ src/test/java/com/keiaa/soko/SokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keiaa.soko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
