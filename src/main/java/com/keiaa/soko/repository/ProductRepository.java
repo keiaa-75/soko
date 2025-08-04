b1dc55de00dfc6fb75c0db7c3bc8888fd4068b7a
Keiaa <confinedrose@duck.com>
2025-08-04 17:53:22 +0800

    Drop redundant methods that have been covered by the service.

--- src/main/java/com/keiaa/soko/repository/ProductRepository.java
+++ src/main/java/com/keiaa/soko/repository/ProductRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.keiaa.soko.model.Product;
 
@@ -28,19 +27,6 @@ import com.keiaa.soko.model.Product;
  * @see com.keiaa.soko.model.Product
  */
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
-    List<Product> findByItemNameContainingIgnoreCase(String itemName);
-
-    @Query("SELECT p FROM Product p WHERE (:category IS NULL OR p.itemCategory = :category) AND (:minPrice IS NULL OR p.itemPrice >= :minPrice) AND (:maxPrice IS NULL OR p.itemPrice <= :maxPrice)")
-    List<Product> filterProducts(@Param("category") String category, @Param("minPrice") Double minPrice, @Param("maxPrice") Double maxPrice);
-
-    List<Product> findAllByOrderByItemPriceAsc();
-    List<Product> findAllByOrderByItemPriceDesc();
-
-    List<Product> findAllByOrderByItemIdAsc();
-    List<Product> findAllByOrderByItemIdDesc();
-    List<Product> findAllByOrderByItemNameAsc();
-    List<Product> findAllByOrderByItemNameDesc();
-
     @Query("SELECT DISTINCT p.itemCategory FROM Product p ORDER BY p.itemCategory")
     List<String> findDistinctItemCategories();
 }
